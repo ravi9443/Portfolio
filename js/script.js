d01fce7725c78b48476a38525e28f1db470eb05c
_ravi9443 <ravigangwal221@gmail.com>
2024-02-13 19:30:10 +0530

    Mail feature is added

--- js/script.js
+++ js/script.js
@@ -45,3 +45,25 @@ window.onscroll =()=>{
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
+
+function sendMail(){
+    var params={
+        name: document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        message:document.getElementById("message").value,
+    };
+    const serviceID="service_cv5qes9";
+const templateID="template_yu0svah";
+
+emailjs.send(serviceID,templateID,params)
+.then(
+    res=>{
+        document.getElementById("name").value="";
+        document.getElementById("email").value="";
+        document.getElementById("message").value="";
+        console.log(res);
+        alert("Your message sent Successfully");
+    })
+    .catch((err)=> console.log(err));
+}
+
